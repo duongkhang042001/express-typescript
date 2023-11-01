4bbe7ff7222969b21e8617267abeb56e4fe9f02e
KhangDuong <khangdt10@fpt.com.vn>
2023-11-01 15:45:56 +0700

    feat: update base code

--- src/server.ts
+++ src/server.ts
@@ -5,18 +5,24 @@ import { connectToDatabase } from "./config/database";
 
 dotenv.config();
 
-const app: Express = express();
-const port = process.env.APP_PORT || 8000;
+const main = () => {
+    const app: Express = express();
+    const port = process.env.APP_PORT || 8000;
 
-app.use("/public", express.static("public"));
+    app.use("/public", express.static("public"));
 
-app.use(express.json());
+    app.use(express.json());
 
-app.get("/", (req, res) => res.send("Welcome to API!"));
-app.use("/v1", routes);
+    app.get("/", (req, res) => res.send("Welcome to API!"));
+    app.use("/v1", routes);
 
-connectToDatabase(String(process.env.MONGODB_URL));
+    connectToDatabase(String(process.env.MONGODB_URL));
 
-app.listen(port, () => {
-  console.log(`⚡️ [Server]: Server is running at http://localhost:${port}`);
-});
+    app.listen(port, () => {
+        console.log(
+            `⚡️ [Server]: Server is running at http://localhost:${port}`
+        );
+    });
+};
+
+export { main };
