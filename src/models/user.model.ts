f7f723a453fdce9afd20e7608421eca995e3ee03
KhangDuong <khangdt10@fpt.com.vn>
2023-11-01 16:07:42 +0700

    feat: update code

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,25 +1,15 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
 
 export interface I_UserDocument extends mongoose.Document {
-  username: string;
-  name: string;
-  password: string;
+    username: string;
+    name: string;
+    password: string;
 }
 
 const UserSchema: mongoose.Schema<I_UserDocument> = new mongoose.Schema({
-  username: { type: String, unique: true },
-  password: { type: String },
-  name: { type: String },
-});
-
-const saltRounds = 8;
-
-UserSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
-    this.password = await bcrypt.hash(this.password, saltRounds);
-  }
-  next();
+    username: { type: String, unique: true },
+    password: { type: String },
+    name: { type: String },
 });
 
 const UserModel = mongoose.model<I_UserDocument>("User", UserSchema);
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,10 +1,10 @@
 import { DocumentDefinition } from "mongoose";
-import UserModel, { I_UserDocument } from "../models/user.model";
+import UserModel, { I_UserDocument } from "@/models/user.model";
 
 export async function login(user: DocumentDefinition<I_UserDocument>) {
-  return await UserModel.findOne({ username: user.username });
+    return await UserModel.findOne({ username: user.username });
 }
 
 export async function register(user: DocumentDefinition<I_UserDocument>) {
-  return await UserModel.findOne({ username: user.username });
+    return await UserModel.findOne({ username: user.username });
 }
